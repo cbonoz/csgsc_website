828e76c026d2d49cd9d6fdd413d3fd8be4bdf6b4
Chris Buonocore <chrisdistrict@gmail.com>
2016-11-05 13:39:00 -0700

    Update image folder ref

--- Gruntfile.js
+++ Gruntfile.js
@@ -379,7 +379,7 @@ module.exports = function (grunt) {
           src: [
             '*.{ico,png,txt}',
             '*.html',
-            'images/{,*/}*.{webp}',
+            'images/{,*/}*.{webp,jpg}',
             'styles/fonts/{,*/}*.*'
           ]
         }, {
